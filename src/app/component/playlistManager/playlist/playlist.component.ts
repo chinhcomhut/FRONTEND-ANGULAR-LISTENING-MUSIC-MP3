1ef032297b5b00361884040950cdba1d25b1cf12
chinhcomhut <chinhcomhut@gmail.com>
2019-12-24 11:09:19 +0700

    Update Da them duoc nhac vao list

--- src/app/component/playlistManager/playlist/playlist.component.ts
+++ src/app/component/playlistManager/playlist/playlist.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {PlaylistInfor} from '../../../model/playlist/playlist-Infor';
 import {PlaylistService} from '../../../services/playlistManager/playlist.service';
 import {ActivatedRoute} from '@angular/router';
+import {Track} from 'ngx-audio-player';
+import {Song} from '../../../model/song/song';
 
 @Component({
   selector: 'app-playlist',
@@ -9,6 +11,16 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./playlist.component.css']
 })
 export class PlaylistComponent implements OnInit {
+    song: Song[];
+    msaapDisplayTitle = true;
+    msaapDisplayPlayList = true;
+    pageSizeOptions = [2, 4, 6];
+    msaapDisplayVolumeControls = true;
+    msbapDisplayTitle = false;
+    msbapDisplayVolumeControls = true;
+    playlist1: Track;
+    playlist2: Track[] = [];
+    msaapPlaylist2: Track[] = [];
 
   title = 'Danh Sách Bài Hát Trong PLaylist Của Tôi';
 
@@ -25,6 +37,15 @@ export class PlaylistComponent implements OnInit {
           next => {
             this.playlist = next;
             console.log(next);
+            this.song = next.songs;
+            for (const song of this.song ) {
+                this.playlist1 = {
+                    title: song.nameSong,
+                    link: song.mp3Url
+                };
+                this.playlist2.push(this.playlist1);
+            }
+            this.msaapPlaylist2 = this.playlist2;
           },
           error => {
             this.playlist = null;
@@ -33,4 +54,24 @@ export class PlaylistComponent implements OnInit {
       );
     });
   }
+    changeMsbapDisplayTitle(event) {
+        this.msbapDisplayTitle = event.checked;
+    }
+
+    changeMsbapDisplayVolumeControls(event) {
+        this.msbapDisplayVolumeControls = event.checked;
+    }
+
+    changeMsaapDisplayTitle(event) {
+        this.msaapDisplayTitle = event.checked;
+    }
+
+    changeMsaapDisplayPlayList(event) {
+        this.msaapDisplayPlayList = event.checked;
+    }
+
+    changeMsaapDisplayVolumeControls(event) {
+        this.msaapDisplayVolumeControls = event.checked;
+    }
+
 }
